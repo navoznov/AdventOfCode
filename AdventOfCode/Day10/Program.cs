a9dfd919360f5dab8bb4a4c219bd6b8611fae2d5
imbeat <ivan.navoznov@gmail.com>
2015-12-23 11:24:14 +0300

    day10 problem is solved

---
+++ AdventOfCode/Day10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            Part1(input);
+            Part2(input);
+        }
+
+        private static void Part2(string input)
+        {
+            GetLookAndSayText(input, 50);
+        }
+
+        private static void Part1(string input)
+        {
+            GetLookAndSayText(input, 40);
+        }
+
+        private static void GetLookAndSayText(string text, int itarationsCount)
+        {
+            var regex = new Regex(@"((\d)\2*)");
+            for (var i = 0; i < itarationsCount; i++)
+            {
+                var matches = regex.Matches(text);
+                var sb = new StringBuilder();
+                for (int j = 0; j < matches.Count; j++)
+                {
+                    var value = matches[j].Groups[0].Value;
+                    sb.Append(value.Length).Append(value[0]);
+                }
+                text = sb.ToString();
+            }
+            Console.WriteLine(text.Length);
+        }
+    }
+}
